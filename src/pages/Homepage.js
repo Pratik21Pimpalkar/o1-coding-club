2bc57822d83a334a12a1655225524222fda7c71c
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2022-09-11 17:49:30 +0530

    Testimonials Added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Hero from '../components/Hero'
 import wave from "../Assets/wave1.svg"
 import Stats from '../components/Stats'
+import Testimonial from '../components/Testimonial'
 const Homepage = () => {
   return (
     <div>
       {/* <Hero /> */}
       <Stats/>
+      <Testimonial/>
     </div>
   )
 }
