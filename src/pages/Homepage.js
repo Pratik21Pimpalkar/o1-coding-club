44618ffadfad55ea3f1f239da195798db25dbccc
Twinshu <twinshup@gmail.com>
2022-09-25 22:06:12 +0530

    responsive done

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import CompanySlider from '../components/CompanySlider'
-import wave from "../Assets/wave1.svg"
 import OurEvents from '../components/OurEvents'
 import Testimonial from '../components/Testimonial'
 import Stats from '../components/Stats'
