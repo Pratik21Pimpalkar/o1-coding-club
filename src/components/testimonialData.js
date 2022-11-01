f5189267593ae34d991d60ecf9ff07677344f964
Twinshu <twinshup@gmail.com>
2022-11-01 20:16:49 +0530

    testimonial data edited

--- src/components/testimonialData.js
+++ src/components/testimonialData.js
@@ -1,46 +1,21 @@
 const testimonialData = [
-    {
-        name: "Akshay Mishra",
-        role: "Data Engineer at Amazon",
-        content: "I started my interview preparation journey from the first seminar of O(1). Things were explained very well in detail, from tips and tricks for interviews to resources for DSA and core subjects.CPL was one of the highlights for me as I was able to improve my problem solving skills significantly. I would recommend O(1) to every fresher looking for mentorship, guidance and a productive atmosphere to learn and grow.",
-        img: "https://drive.google.com/uc?export=view&id=1nX5E-O3P0fGIluXEd_-e2DosOgI4AP6k"
-    },
-    {
-        name: "Amarkumar Mishra",
-        role: "BackEnd Developer  at HackerEarth",
-        content: "I participated in CPL organized by O(1) Coding Club in May 2020 and I was team leader of my team. As, CPL was a team game it not only helped me to enhance my codingskills but also helped me to understand- 1. How to collaboratively solve a problem as a team. 2. Other better approaches for the same problem that you find out after discussing with your team mates. I would highly recommend to participate in CPL contest organized by O(1) coding club.",
-        img: "https://drive.google.com/uc?export=view&id=1ge-6zmMLWXlSkuHZM1FhHIi0EnB93fCH",
-    },
-    {
-        name: "Sakshi Thakur",
-        role: "SDE at VMware",
-        content: "I have been helped a lot from O(1) club! Right from the first seminar, we have been guided in almost everything related to placements. I started participating rigorously in CPL aore subjects. As a fresher guidance and resources helped me to learn and grow.",
-        img: "https://drive.google.com/uc?export=view&id=1UZ-m-lqGNyk5SDDdyvrMSJwv1YeSTyt1"
-    },
+
+  
+    
     {
         name: "Pranav Manbhekar",
         role: "Software Engineering  at Fivetran",
         content: 'CPL and various knowledge sessions helped me in my competitive coding and interview preparation.CPL was quite helpful as it helped to improve competitive coding in a funway and it also improved coding culture. It would help beginners to kickstart their coding journey. Various off-campus opportunities were also posted which can help to avoid missing on opportunities.',
         img: "https://drive.google.com/uc?export=view&id=1eeOqEyWuSa6Tj6yesJyvckNlnvjv1mec"
     },
-    {
-        name: "Bhumika Nagrecha",
-        role: "SDE at Bank of New York Mellon",
-        content: "I started my interview preparation journey from the first seminar of O(1). Things were explained very well in detail, from tips and tricks for interviews to resources for DSA andin contest streaks which improved my coding skills due to which I could clear the off-campus coding test for a job opportunity at Bank Of New York in my 3rd year itself . So in a nutshell, O(1) club is a single point solution for complete placement guidance.",
-        img: "https://drive.google.com/uc?export=view&id=1EzEOaZ5J-oGYS-3tuFBh72xNqqXbfesd",
-    },
+    
     {
         name: "Shrey Agrawal",
         role: "Power Programmer at Infosys",
         content: 'My first experience with O(1) coding club was from their first seminar and since then, I have nothing but good things to say. From their guidance relating to placements, both on and off campus,to their CPL, everything important was explained and helped in becoming better programmers. I was fortunate enough lead a team in CPL, and watching me, my classmates, and juniors improve every contest was a great experience.',
         img: "https://drive.google.com/uc?export=view&id=10guXYDqvOmpcAuW1f36kCyGpnKOEghEJ"
     },
-    {
-        name: "Vishal Kriplani",
-        role: "Software Engineering  at Fivetran",
-        content: "The CPL helped me to maintain the speed and accuracy of problem-solving. Senior-Junior interaction was increased during these events. The Knowledge sessions were quite informative.",
-        img: "https://drive.google.com/uc?export=view&id=1ZukqZ5z6OhL5ibqfalKr4CtsKKOF3Cgd"
-    },
+  
     {
         name: "Sudhanshu Bhogal", role: "SDE-T at Amazon",
         content: "I took part in Coding Premier League and multiple Competitive Coding Streak contests organised by O(1) Coding Club.Before attending those events I was not able to solve even very simplecoding problems on Codechef and Codeforces but after receiving proper guidance on resources and improving thought process for solving problems by O(1) Coding Club, I improved a lot in few months. None of the interviews I gave had gone without solving a coding problem, so I would suggest to improve your competitive coding skill if you are not good in it and I would highly recommend to be part of O(1) Coding Club and take active participation in various contests and sessions organized by them.",
@@ -123,7 +98,7 @@ const testimonialData = [
         With the help of different sessions conducted by O(1) coding club, I was able to bag dream package placements with offers like SDE at Persistent Systems and SDE-T at ZS associates.
         O1 also helped me in my second year to bag summer internship at Microland Ltd. with their guidance.
         Coderun helped me a lot to prepare for the placement season.`,
-        img: "images/aditya.jpeg",
+        img: "https://drive.google.com/uc?export=view&id=1ici6-DvAE-I7Aqwwn7K77BRaeH8psmIR/view",
         linkedin: "https://www.linkedin.com/in/aditya-sharma-0b21a3197/",
       },
       {
@@ -150,7 +125,7 @@ const testimonialData = [
         name: "Ashwin Walunj ",
         role:" PP @ Infosys",
         content: `So I joined the program called CodeRun conducted by the O(1) coding club, where we were provided with ladders of different levels and after every 3 days, we had doubt clearance sessions. What I understood was to trust the process and keep on enhancing your knowledge and expertise on the basics just combine this with a little bit of luck and you will also land a great job. Finally, I cleared the HackWithInfy and banged an offer for the role of power programmer. If it weren't for the O(1) coding club, I would've known this too late and might not have landed the job that I have now.`,
-        img: "images/ashwinalunj.jpg",
+        img: "https://drive.google.com/uc?export=view&id=1ici6-DvAE-I7Aqwwn7K77BRaeH8psmIR",
         linkedin: "https://www.linkedin.com/in/atharva-u-96436a191/",
       },
       {
