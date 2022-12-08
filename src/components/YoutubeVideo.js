71c728745d8ff87de734d542ca433713005d7b68
Twinshu <twinshup@gmail.com>
2022-12-09 00:46:37 +0530

    embedded video improved

--- src/components/YoutubeVideo.js
+++ src/components/YoutubeVideo.js
@@ -5,11 +5,12 @@ const YoutubeVideo = () => {
     <YoutubeWrapper>
       <div className="embeded-video">
         <iframe
-          src="https://www.youtube.com/embed/ylVJ4mP6sTc"
+          src="https://www.youtube.com/embed/ylVJ4mP6sTc?autoplay=1&mute=1"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
+          autoPlay="1"
         ></iframe>
       </div>
     </YoutubeWrapper>
